f0a3f98387b298210a5b6a1444025c1b832a3de6
Andrew Frank <=>
2016-12-27 09:28:20 -0500

    Add section header for each day
    There are A LOT of sessions at CodeMash this year! To make it easier to scan through the large list of sessions for a given day, I added a section header to the list so it can more easily be identified what day the session takes place on.

--- src/CodeMashSchedule/App/Containers/App.js
+++ src/CodeMashSchedule/App/Containers/App.js
@@ -11,8 +11,10 @@ export default class App extends Component {
 
   constructor() {
       super()
+
       const ds = new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
+        rowHasChanged: (r1, r2) => r1 !== r2,
+        sectionHeaderHasChanged: (s1, s2) => s1 !== s2
       })
 
       this.state = {
@@ -32,6 +34,14 @@ export default class App extends Component {
     )
   }
 
+  _renderSectionHeader(sectionData, sectionID) {
+    return (
+      <View style={styles.sectionHeader}>
+        <Text style={styles.sectionHeaderText}>Day {sectionID}</Text>
+      </View>
+    )
+  }
+
   _renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
     let key = sectionID + ":" + rowID
 
@@ -47,11 +57,26 @@ export default class App extends Component {
 
     fetch('https://speakers.codemash.org/api/SessionsData')
       .then(response => response.json())
+      .then(sessions => {
+        return sessions.reduce((sessionsByDay, session) => {
+          let sessionStartTime = new Date(session.SessionStartTime)
+          let day = sessionStartTime.getDate() - 9
+          let sessions = sessionsByDay[day] || []
+
+          sessions.push(session)
+          sessionsByDay[day] = sessions
+
+          return sessionsByDay
+        }, {})
+      })
       .then(sessions => this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(sessions)
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(sessions)
       }))
       .then(() => this.toggleLoadingIndicator())
-      .catch(error => console.log(error))
+      .catch(error => {
+        console.log(error)
+        throw error
+      })
   }
 
   render() {
@@ -66,6 +91,7 @@ export default class App extends Component {
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this._renderRow}
+          renderSectionHeader={this._renderSectionHeader}
           renderSeparator={this._renderSeparator} />
       </View>
     );
@@ -82,7 +108,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
-    paddingTop: 25
+    paddingTop: 25,
   },
   loadingIndicator: {
     height: 80
@@ -92,6 +118,16 @@ const styles = StyleSheet.create({
     opacity: .3,
     borderWidth: 1,
     marginHorizontal: 10,
+  },
+  sectionHeader: {
+    backgroundColor: 'darkorange',
+    padding: 5,
+  },
+  sectionHeaderText: {
+    color: '#F5FCFF',
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
   },
   session: {
     padding: 10
